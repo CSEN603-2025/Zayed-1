bfa8cae716e34d72b8f90c002b6e74431a5d108c
MazenA14 <mazen.ahmed05@gmail.com>
2025-05-06 18:50:08 +0300

    Initializing React Router

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## Zayed-1
+
+#### Steps to run
+* Run this in the guc-internship-system directory `npm install`
+* Start App using `npm run dev`
---
+++ guc-internship-system/app/routes/home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h2>Welcome to the GUC Internship System</h2>
+    </div>
+  );
+}
