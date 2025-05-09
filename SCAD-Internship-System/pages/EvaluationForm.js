68f2f28768be5f9829687a50a0f151367303a9f4
MazenA14 <mazen.ahmed05@gmail.com>
2025-05-10 02:20:54 +0300

    Added Evaluation form & FacultyFeedback

---
+++ SCAD-Internship-System/pages/EvaluationForm.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+import Navbar from '../components/Navbar';
+
+const PageContainer = styled.div`
+  min-height: 100vh;
+  background-color: ${props => props.theme.colors.light};
+`;
+
+const ContentContainer = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+const Header = styled.div`
+  margin-bottom: 2rem;
+`;
+
+const Title = styled.h1`
+  color: ${props => props.theme.colors.primary};
+  font-size: 2rem;
+  margin-bottom: 0.5rem;
+`;
+
+const Description = styled.p`
+  color: ${props => props.theme.colors.secondary};
+  font-size: 1rem;
+`;
+
+const EvaluationForm = () => {
+  return (
+    <PageContainer>
+      <Navbar userType="company" />
+      
+      <ContentContainer>
+        <Header>
+          <Title>Intern Evaluation</Title>
+          <Description>Evaluate intern performance and provide feedback</Description>
+        </Header>
+        
+        <div>
+          <p>This page is under construction.</p>
+        </div>
+      </ContentContainer>
+    </PageContainer>
+  );
+};
+
+export default EvaluationForm; 
