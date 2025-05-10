2cc8393cc6b526625d51948bf2a2a163f2de8415
abobakr-hassan <abobakrhossam2005@gmail.com>
2025-05-10 14:03:16 +0300

    Report Abobakr

---
+++ SCAD-Internship-System/src/pages/ReportList.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+import Navbar from '../components/Navbar';
+
+const PageContainer = styled.div`
+  min-height: 100vh;
+  background-color: ${props => props.theme.colors.light};
+`;
+
+const ContentContainer = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+const Header = styled.div`
+  margin-bottom: 2rem;
+`;
+
+const Title = styled.h1`
+  color: ${props => props.theme.colors.primary};
+  font-size: 2rem;
+  margin-bottom: 0.5rem;
+`;
+
+const Description = styled.p`
+  color: ${props => props.theme.colors.secondary};
+  font-size: 1rem;
+`;
+
+const ReportList = () => {
+  return (
+    <PageContainer>
+      <Navbar userType="scadOffice" />
+      
+      <ContentContainer>
+        <Header>
+          <Title>Internship Reports</Title>
+          <Description>View and manage all student internship reports</Description>
+        </Header>
+        
+        <div>
+          <p>This page is under construction.</p>
+        </div>
+      </ContentContainer>
+    </PageContainer>
+  );
+};
+
+export default ReportList; 
